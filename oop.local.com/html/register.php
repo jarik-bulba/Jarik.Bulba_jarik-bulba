8e79cbeb72d28be58011d95cc18830eb20b2ff9e
jarik-bulba <jarik-bulba@rambler.ru>
2016-11-17 16:08:11 +0200

    add gitignore

--- oop.local.com/html/register.php
+++ oop.local.com/html/register.php
@@ -13,7 +13,9 @@ require_once 'includes/global.inc.php';
     <title>Register</title>
 </head>
 <body>
-<?php echo ($error != "") ? $error : ""; ?>
+
+
+
 <form action="register.php" method="POST">
     <ul>
         <li>
